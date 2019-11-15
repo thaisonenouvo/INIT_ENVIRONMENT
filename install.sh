0b829b6cfac808be1c1a84cda925c17b363072f2
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2019-11-15 17:22:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
   
 install_oh_my_zsh() {
-  echo "Setting up zsh..." 
-  && cd ~
-  && git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-  && git clone https://github.com/zsh-users/zsh-completions#oh-my-zsh
-  && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  echo "Setting up zsh..." \
+  && cd ~ \
+  && git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" \
+  && git clone https://github.com/zsh-users/zsh-completions#oh-my-zsh \
+  && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
+  && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
+  && echo "Finished dowload package plugin for zshrc."
 }
 
 install_github(){
-  echo "Setting up github..." 
+  echo "Setting up github..." \
+  && echo "Finished setting for github."
 }
 
 
 install_homebrew() {
-  echo "Setting up homebrew..." 
+  echo "Setting up homebrew..." \
   # && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
   # && brew install yarn \
   # && brew install watchman 
@@ -23,15 +25,16 @@ install_homebrew() {
   # && brew install qt5  \
   # && pip3 install qutebrowser \
   # && brew install neovim \ 
-  # && brew uninstall --force postgresql
-  # && rm -rf /usr/local/var/postgres
-  # && brew install postgres
+  # && brew uninstall --force postgresql \
+  # && rm -rf /usr/local/var/postgres \
+  # && brew install postgres \
   # echo "Successing to setting homebrew..." 
 }
 install_npm() {
+  echo "Setting up package npm global..." \
+  && npm install -g eslint \
+  && npm install -g react-native-cli \
   echo "Setting up package npm global..." 
-  # && npm install -g eslint
-  # && npm install -g react-native-cli 
 }
 
 install_neovim() {
@@ -44,12 +47,13 @@ install_neovim() {
 
 
 if [[ -z $1 ]]; then
-  echo "This will delete all your previous nvim, zsh settings. Proceed? (y/n)? "
+  echo "This will delete all your previous homebrew, nvim, zsh settings. Proceed? (y/n)? "
   read answer
   if echo "$answer" | grep -iq "^y" ;then
     echo "Installing dependencies..." \
-    && install_oh_my_zsh 
-    && echo "Finished dowload package plugin for zshrc."
+    && install_homebrew \
+    && install_npm \
+    && install_oh_my_zsh \
     && install_neovim \
     && echo "Finished installation."
   fi
