8e09be0c04ab2c997821045dfb1473f3ddbbdfcd
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-03-26 13:08:27 +0700

    brew install diff-so-fancy

--- step1.sh
+++ step1.sh
@@ -24,6 +24,7 @@ install_homebrew() {
   && rm -rf /usr/local/var/postgres \
   && brew install postgres \
   && brew install nvm \
+  && brew install diff-so-fancy \
   && brew install exa \
   && brew update \
   && brew tap caskroom/cask \
